3a0a4e2cb2b425b9bd436853d40cf086212b846b
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-03-04 09:34:00 -0600

    Added Cube file

---
+++ src/Sona_AssignmentArrayFunction/Cube.java
@@ -0,0 +1,15 @@
+package Sona_AssignmentArrayFunction;
+
+public class Cube {
+
+    public static void main(String[] args) {
+        cube();
+    }
+
+    public static void cube(){
+        int number = 3;
+        int cube= number*number*number;
+        System.out.println("Cube of the number is " + cube);
+
+    }
+}
