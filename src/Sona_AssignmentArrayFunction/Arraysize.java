60fa71fa3326b2bddd0a27444085ccdb4de5e7a6
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-03-04 23:40:06 -0600

    New files commited

---
+++ src/Sona_AssignmentArrayFunction/Arraysize.java
@@ -0,0 +1,12 @@
+package Sona_AssignmentArrayFunction;
+
+public class Arraysize {
+
+    public static void main(String[] args) {
+
+        int num [] = {1, 2, 3, 5, 4, 6, 7, 8, 8, 9, 10};
+
+        System.out.println("The size of this array is "+ num.length +".");
+    }
+
+}
